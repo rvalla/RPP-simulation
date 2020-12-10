86b3f10be2de536d1030536d70a08a906c330368
Rodrigo Valla <rodrigovalla@protonmail.ch>
2020-12-10 16:13:24 -0300

    Simulating level 3 from PPP game...

--- simulation.py
+++ simulation.py
@@ -1,5 +1,6 @@
 #Triggering a new Rock, Paper & Scissors simulation
 from ppp import PPP
 
-g = PPP("config/level2_dice1.json")
-g = PPP("config/level2_dice2.json")
+g = PPP("config/level3_dice1.json")
+g = PPP("config/level3_dice2.json")
+g = PPP("config/level3_dice3.json")
